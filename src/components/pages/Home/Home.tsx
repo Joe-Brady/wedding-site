440d015b82155a3c3c9fa842811e7434d071fd45
Joe Brady <joe.brady@lytt.tech>
2021-05-06 08:47:31 +0100

    Add custom JS form submission success

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,38 +1,47 @@
-import React, { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import React, { ReactElement, FormEvent } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import { PageUrl } from "../../../types";
 import Page from "../../templates/Page/Page";
 import Typography from "../../core/Typography/Typography";
 
-const Home = (): ReactElement => (
-  <Page
-    head={<Typography variant="h1" text="Jasmine and Joseph" />}
-    headImageSrc=""
-  >
-    <div>
-      <form name="RSVP" method="POST" action="/rsvp-success">
-        <input type="hidden" name="form-name" value="RSVP" />
-        <label>
-          Can you attend?
-          <select name="Attending">
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-        </label>
-        <label>
-          How many people can attend?
-          <input type="number" name="Number of people" min="0" max="20" />
-        </label>
-        <label>
-          What are your full names?
-          <input type="text" name="Names" />
-        </label>
-        <button type="submit">Send RSVP</button>
-      </form>
-      <Link to={PageUrl.Home}>Homepage link</Link>
-    </div>
-  </Page>
-);
+const Home = (): ReactElement => {
+  const history = useHistory();
+
+  function submitRsvp(event: FormEvent) {
+    event.preventDefault;
+    history.push(PageUrl.RsvpSuccess);
+  }
+
+  return (
+    <Page
+      head={<Typography variant="h1" text="Jasmine and Joseph" />}
+      headImageSrc=""
+    >
+      <div>
+        <form name="RSVP" method="POST" onSubmit={submitRsvp}>
+          <input type="hidden" name="form-name" value="RSVP" />
+          <label>
+            Can you attend?
+            <select name="Attending">
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </select>
+          </label>
+          <label>
+            How many people can attend?
+            <input type="number" name="Number of people" min="0" max="20" />
+          </label>
+          <label>
+            What are your full names?
+            <input type="text" name="Names" />
+          </label>
+          <button type="submit">Send RSVP</button>
+        </form>
+        <Link to={PageUrl.Home}>Homepage link</Link>
+      </div>
+    </Page>
+  );
+};
 
 export default Home;
