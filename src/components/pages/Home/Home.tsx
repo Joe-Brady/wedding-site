51324fcb34fc5f6ad34b518ba99496ae95b632f2
Joe Brady <joe.brady@lytt.tech>
2021-05-06 07:53:41 +0100

    JSX form workaround trial 1

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -12,33 +12,24 @@ const Home = (): ReactElement => (
   >
     <div>
       <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <input type="hidden" name="form-name" value="contact" />
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+        <label>
+          Your Role:{" "}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+        <button type="submit">Send</button>
       </form>
       <Link to={PageUrl.Home}>Homepage link</Link>
     </div>
